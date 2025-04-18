450bd64cea4fb0cce45d017d145c1150022d45fc
Thiều Thắng <126961774+ImPeonyy@users.noreply.github.com>
2025-04-18 16:46:54 +0700

    Update axiosClient.js

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://webcomics-be.fwh.is/api',
+    baseURL: 'https://webcomics-be.fwh.is/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
